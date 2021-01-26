28b045bc4756dd2575886269cddf8c1b82daa907
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2021-01-25 19:44:30 -0600

    Update wires.js

--- src/wires.js
+++ src/wires.js
@@ -25,9 +25,10 @@ export default ({ state, pages, rewrites }) => {
   listener('popstate', routeHandler)
   listener('pushstate', routeHandler)
 
+  // Just for fun benchmark. Results shouldn't be taken seriously.
   listener('DOMContentLoaded', () => {
     const date = Date.now()
-    const html = document.getElementsByTagName('html')[0].outerHTML
+    const html = document.documentElement.outerHTML
 
     const benchmark = {
       ms: date - window._ms,
@@ -35,8 +36,10 @@ export default ({ state, pages, rewrites }) => {
     }
 
     dispatch(() => ({ benchmark }))
+  })
 
-    // ensure that gtm.js never affects load time performance
+  // Ensure that gtm.js never affects load time performance.
+  listener('load', () => {
     if (PROD === true) {
       const script = document.createElement('script')
 
