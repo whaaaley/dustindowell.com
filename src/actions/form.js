501904ebfad11428abc1643a7230c8908a2e3a0f
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2021-03-08 22:47:44 -0600

    Update form.js

--- src/actions/form.js
+++ src/actions/form.js
@@ -27,6 +27,10 @@ export const send = ({ formData }, x) => dispatch => {
   console.log(options)
 
   fetch('/', options)
+    .then(x => {
+      console.log(x)
+      return x
+    })
     .then(res => res.json())
     .then(data => {
       dispatch(replace, {
