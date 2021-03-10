bc2965ea14b518d4d24147cfd838aced33fa7981
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2021-03-10 15:37:28 -0600

    Clean

--- src/test.js
+++
@@ -1,6 +0,0 @@
-
-const testTabs = [
-  { action: 'interface.changeTabs', data: 'stats' },
-  { action: 'interface.changeTabs', data: 'hashtags' },
-  { action: 'interface.changeTabs', data: 'combinations' }
-]
