c5bc58861fe6c204501a3b182ff2779375b7218d
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2021-04-22 03:35:12 -0500

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -71,7 +71,7 @@ const Home = (state, dispatch) => {
         ]),
         Tooltip({
           class: '-left',
-          content: 'This video uses the webm video format. It\'s file size clocks in at tiny 216kB. By contrast the backup mp4 is gigantic 2097kB.'
+          content: 'This video uses the webm video format. It\'s file size clocks in at tiny 216 kB. By contrast the backup mp4 is gigantic 2097 kB.'
         }),
         Tooltip({
           class: '-right',
@@ -101,7 +101,7 @@ const Home = (state, dispatch) => {
       ]),
       Tooltip({
         class: '-left',
-        content: `This app's HTML, CSS, and JS is only ${kb}kB after being unzipped! With gzip compression it's around 8kB! That's impressive considering this app uses flux architecture and virtual DOM for the view layer. For comparison React alone is 40kB gzipped and Vue is 21kB gzipped.`
+        content: `This app's HTML, CSS, and JS is only ${kb} kB after being unzipped! With gzip compression it's around 8kB! That's impressive considering this app uses flux architecture and virtual DOM for the view layer. For comparison React alone is 40 kB gzipped and Vue is 21 kB gzipped.`
       })
     ])
   ])
