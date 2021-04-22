cc5e44d0f849be83d69ca2c8fd0e4a60437829a1
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2021-04-21 20:00:19 -0500

    Preload fonts

--- src/index.js
+++ src/index.js
@@ -10,15 +10,28 @@ const scripts = PROD === true
   ? script(readFileSync('./public/app.js', 'utf8'))
   : script({ defer: true, src: '/app.js' })
 
+const PreloadFont = props =>
+  link({ rel: 'preload', href: props.href, as: 'font', type: 'font/woff2' })
+
 const render = data => {
   return html({ lang: 'en' }, [
     meta({ charset: 'utf-8' }),
     script('window._ms = Date.now()'),
+
     title(data.title),
     meta({ name: 'author', content: data.author }),
     meta({ name: 'description', content: data.description }),
     meta({ name: 'viewport', content: data.viewport }),
+
     link({ rel: 'icon', href: '/cache/favicon.svg' }),
+
+    PreloadFont({ href: '/fonts/Goldbill-XLLight.woff2' }),
+    PreloadFont({ href: '/fonts/Goldbill-XSExtraBold.woff2' }),
+    PreloadFont({ href: '/fonts/MontExtraLightDEMO.woff2' }),
+    PreloadFont({ href: '/fonts/MontHeavyDEMO.woff2' }),
+    PreloadFont({ href: '/fonts/Inter-3.18/Inter-roman.var.woff2' }),
+    PreloadFont({ href: '/fonts/Inter-3.18/Inter-italic.var.woff2' }),
+
     styles,
     body([
       noscript('Please enable JavaScript and try again.'),
