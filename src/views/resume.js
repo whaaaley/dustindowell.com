1c1b60c11c4c92a99dfe3a6fc3f740546519667f
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2023-08-23 17:01:00 -0500

    Update the old site

--- src/views/resume.js
+++ src/views/resume.js
@@ -12,30 +12,42 @@ const Header = () => {
     Text(h1, 'Dustin Dowell'),
     div([
       Text(h2, 'Frontend Engineer'),
-      Text(h2, 'Des Moines, Iowa 50309'),
+      Text(h2, 'Des Moines, Iowa 50311'),
       Text(h2, 'dustindowell22@gmail.com'),
       Text(h2, '515-689-5648')
     ])
   ])
 }
 
-/*
 const Grid = () => {
   return div({ class: 'grid' }, [
     div({ class: 'grid-item' }, [
       Text(h1, 'Experience'),
 
-      Text(h2, 'Lead Frontend Engineer'),
-      Text(h3, 'Alqen - Des Moines, Iowa'),
-      Text(h3, 'June 2021 - Present'),
+      Text(h2, 'Lead Engineer (Remote)'),
+      Text(h3, 'Udundi - Austin, Texas'),
+      Text(h3, 'May 2022 - Current'),
       ul([
-        Text(li, 'Maintained an existing Vue 2 application fixing bugs and adding features.'),
-        Text(li, 'Designed and implemented a completely new Vue 3 application that wraps the pieces of the legacy dashboard as part of an incremental adoption strategy.'),
-        Text(li, 'Ported legacy Vue 2 code to the new Vue 3 application.'),
-        Text(li, 'Implemented onboarding and payment flows for new users and subscriptions.'),
-        Text(li, 'Wrote a SSE (Server Sent Events) server in Node that receives miscellaneous events from our API service and forwarded them to the frontend.'),
-        Text(li, 'Created a user impersonation system using JWTs for administrators.'),
-        Text(li, 'Took a leadership role and helped the team with strategizing and organizing our application at all levels.')
+        Text(li, 'Developed the frontend for an A/B testing and analytics dashboard for Shopify store owners using Vue3, Pinia, Apollo, Tailwind, and Vite.'),
+        Text(li, 'Setup automatic deployment processes for all of our projects using Github Actions and AWS.'),
+        Text(li, 'Worked extensively on our backend services using Node, Mongo, Apollo, and GrapQL to add functionality.'),
+        Text(li, 'Worked on bringing previously unfinished projects using Vue2, Node, and MongoDB across the finish line.'),
+        Text(li, 'Developed previously unfinished Shopify extensions across the finish line.'),
+        Text(li, 'Developed multiple landing pages for previously unfinished projects and products in Vue3.'),
+        Text(li, 'Developed the frontend for a shipping protection dashboard targeted at Shopify store owners.'),
+        Text(li, 'Configured a large portion of AWS to run our applications. Including Elastic beanstalk, Serverless Functions with Lambda, S3, Cognito, API Gateway, etc')
+      ]),
+
+      Text(h2, 'Lead Frontend Engineer (Remote)'),
+      Text(h3, 'Alqen - Las Vegas, Nevada'),
+      Text(h3, 'June 2021 - June 2022'),
+      ul([
+        Text(li, 'Developed multiple internal dashboards in both Vue2 and Vue3 for a variety of automated dropshipping products.'),
+        Text(li, 'Developed realtime events services (backend) that integrated into our existing backend and wrote frontend features to display those realtime events.'),
+        Text(li, 'Modernized and ported old Vue2 dashboards to Vue3.'),
+        Text(li, 'Designed strategies to incrementally adopt Vue3 while maintaining the existing Vue2 project.'),
+        Text(li, 'Developed custom integrations for payment methods using Walmart and Stripe APIs.'),
+        Text(li, 'Created robust impersonation features for product admins to act on behalf of individual users to offer technical support.')
       ]),
 
       Text(h2, 'Frontend Developer & Designer'),
@@ -45,6 +57,33 @@ const Grid = () => {
         Text(li, 'Learning and creating new apps, libraries, and frameworks.'),
         Text(li, 'Various freelance development and design.')
       ]),
+
+
+
+
+
+
+      Text(h1, 'Design'),
+      Text(h2, 'Web Design'),
+      ul([
+        Text(li, 'Deep understanding of how to design responsive and adaptive mobile-first web apps.'),
+        Text(li, 'Knowledge of how to create and organize UI kits and implement style guidelines.'),
+        Text(li, 'Love for minimal CSS.')
+      ]),
+      Text(h2, 'Graphic Design'),
+      ul([
+        Text(li, 'Great sense of design and acute attention to detail.'),
+        Text(li, 'Lots of experience designing for both print and digital.')
+      ]),
+
+
+
+    ]),
+    div({ class: 'grid-item' }, [
+
+
+
+
 
       Text(h2, 'Senior Software Developer'),
       Text(h3, 'Access Publishing Inc. - Johnston, Iowa'),
@@ -75,45 +114,28 @@ const Grid = () => {
         Text(li, 'Converted existing static web designs into WordPress, Drupal, Sitefinity, and Joomla.'),
         Text(li, 'Designed and developed original websites, business cards, logos, infographics, and other printed and digital documents.'),
         Text(li, 'Added features, tweaked designs, and updated content on existing apps.')
-      ])
-    ]),
-    div({ class: 'grid-item' }, [
+      ]),
+
+
+
+
+
+
+
       Text(h1, 'Technical'),
 
-      Text(h2, 'Languages & Frameworks'),
-      ul([
-        Text(li, 'Extensive knowledge of JavaScript and JS frameworks like Vue, React, Hyperapp, and Mithril as well as their ecosystems.'),
-        Text(li, 'Experience with Node.js and some of it\'s popular frameworks like Express, Koa, Micro.'),
-        Text(li, 'Experience using platforms like Netlify, Vercel, Heroku, Serverless, Hasura, Supabase, and many more.'),
-        Text(li, 'Experience writing GraphQL APIs on Hasura.'),
-        Text(li, 'Lots of recent experience writing file servers and SSE servers using Deno!'),
-        Text(li, 'Some experience using TypeScript.'),
-        Text(li, 'Hello world\'s completed in Ruby, Python, Elm, and Rust.'),
-        Text(li, 'Tons of experience with CSS and CSS preprocessors.')
-      ]),
-      Text(h2, 'Creative Software'),
+      Text(h2, 'Software'),
       ul([
         Text(li, 'Experienced with all kinds of creative software like Blender, Autodesk 3ds Max, Photoshop, Illustrator, After Effects, Procreate, ArtStudio Pro, Medibang, and more.')
       ]),
-      Text(h2, 'Content Management'),
+      Text(h2, 'Languages'),
       ul([
-        Text(li, 'Lots of experience with database as a service systems like Hasura, MongoDB, Supabase, and some experience with Firebase.'),
-        Text(li, 'Experience with all the classic systems like WordPress, Drupal, Sitefinity, and Joomla.'),
-        Text(li, 'Some experience with less popular miscellaneous systems like Netlify CMS, Grav CMS, Pico, Kirby, and Craft.')
+        Text(li, 'Extensive knowledge of JavaScript and JS frameworks.'),
+        Text(li, 'Some experience using TypeScript.'),
+        Text(li, 'Hello world\'s completed in SBCL Lisp, Go, Ruby, Python, Elm, and Rust.'),
+        Text(li, 'Tons of experience with CSS preprocessors like Sass, Less, Stylus, and PostCSS.')
       ]),
 
-      Text(h1, 'Design'),
-      Text(h2, 'Web Design'),
-      ul([
-        Text(li, 'Deep understanding of how to design responsive and adaptive mobile-first web apps.'),
-        Text(li, 'Knowledge of how to create and organize UI kits and implement style guidelines.'),
-        Text(li, 'Love for minimal CSS.')
-      ]),
-      Text(h2, 'Graphic Design'),
-      ul([
-        Text(li, 'Great sense of design and acute attention to detail.'),
-        Text(li, 'Lots of experience designing for both print and digital.')
-      ]),
 
       Text(h1, 'Education'),
       Text(h2, 'Associate of Arts & Sciences'),
@@ -125,25 +147,24 @@ const Grid = () => {
       Text(h2, 'High School Diploma'),
       Text(h3, '2008 - 2012'),
       Text(h3, 'Southeast Warren Jr. / Sr. High School')
+
+
     ])
   ])
 }
 
-console.log(JSON.stringify(Grid()))
-*/
-
 const Resume = (state, dispatch) => {
   return div({ class: 'resume' }, [
     Header(),
-    // Grid()
-    (() => {
-      if (state.resume.success === null) {
-        return div({ class: 'spinner-box' }, [
-          div({ class: '_spinner' })
-        ])
-      }
-      return state.resume.data
-    })()
+    Grid()
+    // (() => {
+    //   if (state.resume.success === null) {
+    //     return div({ class: 'spinner-box' }, [
+    //       div({ class: '_spinner' })
+    //     ])
+    //   }
+    //   return state.resume.data
+    // })()
   ])
 }
 
